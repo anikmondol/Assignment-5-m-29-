2c11e4e85b6061aa0d872ed1ca0f797c25d22bd3
anikmondol <anikmondol558363@gmil.com>
2024-04-08 18:08:29 +0600

    added code

--- js/index.js
+++ js/index.js
@@ -118,8 +118,7 @@ for (const seat of allSeat) {
     setInnerText('count_total', count);
 
 
-    const number = getARandomAlphabet(1);
-    console.log(number);
+    
 
 
     
@@ -178,17 +177,3 @@ function openTheHomePage(){
 // -------------------------
 
 
-
-function getARandomAlphabet(){
-    // get or create an alphabet array
-    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetString.split('');
-    
-    // get a random index between 0 - 25
-    const randomNumber = Math.random()*25;
-    const index = Math.round(randomNumber);
-   
-
-    const alphabet = alphabets[index];
-    return alphabet;
-}
